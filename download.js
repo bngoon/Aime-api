2e9138c92b6b421db3ef05d8e086923ee21b033f
Booker Ngoon <bookerngoon@gmail.com>
2024-03-05 10:42:11 -0500

    data loaded and model completed

---
+++ download.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch'
+import {promises as fsPromises} from 'fs'
+
+const url = 'https://narutodb.xyz/api/character?page=1&limit=20';
+const headers = {
+    'accept': 'application/json'
+};
+
+fetch(url, { headers })
+    .then(response => response.json())
+    .then(data => fsPromises.writeFile("./data.json", JSON.stringify(data)))
+    .catch(error => console.error(error));
---
+++ models/characters.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+let characterSchema = new Schema (
+    {
+        "name": {type: String},
+        "images":[{ type: String }],
+        "birthdate": {type: String},
+        "sex": {type: String},
+        "status": {type: String},
+
+
+
+    }
+)
+
+export default mongoose.model("characters", characterSchema)
