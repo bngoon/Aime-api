b3d6ea68169e8bb7fc4c3fc2a6d78af6d72398fc
Booker Ngoon <bookerngoon@gmail.com>
2024-03-05 18:30:10 -0500

    vp

--- models/Character.js
+++ models/Character.js
@@ -2,29 +2,17 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema
 
-let characterSchema = new Schema (
-    {
-        "name": {type: String},
-        "images":[{ type: String }],
-        "jutsu": [String],
-        "personal": {
-        "birthdate": {type: String},
-        "sex": {type: String},
-        "status": {type: String},
-        }
+let characterSchema = new Schema({
+    "name": { type: String },
+    "images": [{ type: String }],
+    "jutsu": [String],
+    "personal": {
+        "birthdate": { type: String },
+        "sex": { type: String },
+        "status": { type: String }
     }
-)
+});
 
 export default mongoose.model("characters", characterSchema)
 
 
-// "personal": {
-//     "birthdate": "December 1",
-//     "sex": "Male",
-//     "status": "Deceased",
-//     "height": { "Part II": "210.1cm" },
-//     "weight": { "Part II": "97.2kg" },
-//     "bloodType": "O",
-//     "occupation": "Raikage",
-//     "affiliation": "Kumogakure"
-//   },
