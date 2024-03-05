8a5b032470685b5803673dc98f7f79a16cbd8ed5
Booker Ngoon <bookerngoon@gmail.com>
2024-03-05 18:24:34 -0500

    MVP

--- models/Character.js
+++ models/Character.js
@@ -6,11 +6,25 @@ let characterSchema = new Schema (
     {
         "name": {type: String},
         "images":[{ type: String }],
+        "jutsu": [String],
+        "personal": {
         "birthdate": {type: String},
         "sex": {type: String},
         "status": {type: String},
-        "jutsu": [String]
+        }
     }
 )
 
 export default mongoose.model("characters", characterSchema)
+
+
+// "personal": {
+//     "birthdate": "December 1",
+//     "sex": "Male",
+//     "status": "Deceased",
+//     "height": { "Part II": "210.1cm" },
+//     "weight": { "Part II": "97.2kg" },
+//     "bloodType": "O",
+//     "occupation": "Raikage",
+//     "affiliation": "Kumogakure"
+//   },
